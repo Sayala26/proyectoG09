70c7ab32f809f5c3824f3d2d06ef16f677b47cb1
Sayala26 <sayala79@unisalle.edu.co>
2022-09-02 22:51:50 -0500

    cambiar header

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="header">
+    <h1 class="hola">Bienvenido al sistema de inventario de</h1>
+    <h2 class="hola">"A la brasa"</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderApp'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+  .hola {
+    color: rgb(255, 255, 255);
+  }
+  #header {
+    background-color: #533908;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+  }
+</style>
