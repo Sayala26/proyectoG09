d0f7af52c76b910b933ecabad57528219972dd9c
Angie Obando <angieobandoa@gmail.com>
2022-09-26 19:08:08 -0400

    CRUD Aliemntos/Url separadas

--- authProject/urls.py
+++ authProject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from authApp import views
 from authApp.views.cocina_view.cocinaViewSet import CocinaViewSet
+from authApp.views.alimento_view.alimentoViewSet import AlimentoViewSet
 from .router import router
 
 
@@ -15,7 +16,11 @@ urlpatterns = [
     path('user/', views.UsuarioCreateView.as_view()), # crear usuario
 
     # URL ANGIE
-    path('alimentos/', include(router.urls)), #CRUD Alimentos
+    #path('alimentos/', include(router.urls)), #CRUD Alimentos
+    path('Alimento/<int:pk>', AlimentoViewSet.as_view({'get':'retrieve', 'patch':'partial_update', 'put': 'update'})),
+    path('Alimento/list', AlimentoViewSet.as_view({'get':'list'})),
+    path('Alimento/create', AlimentoViewSet.as_view({'post':'create'})),
+    path('Alimento/delete/<int:pk>', AlimentoViewSet.as_view({'delete':'destroy'})),
 
     #URLS ALEJANDRA
     path('cocina/<int:pk>', CocinaViewSet.as_view({'get':'retrieve', 'patch':'partial_update', 'put': 'update'})),
