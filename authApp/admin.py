04ba80b93ba9950b9247bbdcccf174d6af71fbaf
Sayala26 <sayala79@unisalle.edu.co>
2022-09-27 20:23:03 -0500

    vista corregida

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models.alimento import Alimento
+from .models.limpieza import Limpieza
 
 # Register your models here.
--- authProject/router.py
+++ authProject/router.py
@@ -1,6 +1,10 @@
+
 from authApp.views.alimento_view.alimentoViewSet import AlimentoViewSet
 from rest_framework import routers
+from authApp.views.limpieza_view.limpiezaViewSet import LimpiezaViewSet
+
 
 router = routers.DefaultRouter()
 router.register('alimento', AlimentoViewSet)
+router.register('limpieza',LimpiezaViewSet)
 
