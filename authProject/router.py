c307458ab367806479be1d159cee359a0b77eb6b
Brandon <xevazz116@gmail.com>
2022-09-29 09:30:07 -0500

    actualizacion de vista pero en router ...

--- authProject/router.py
+++ authProject/router.py
@@ -2,9 +2,11 @@
 from authApp.views.alimento_view.alimentoViewSet import AlimentoViewSet
 from rest_framework import routers
 from authApp.views.limpieza_view.limpiezaViewSet import LimpiezaViewSet
+from authApp.views.proveedor_view.proveedorViewSet import ProveedorViewSet
 
 
 router = routers.DefaultRouter()
 router.register('alimento', AlimentoViewSet)
 router.register('limpieza',LimpiezaViewSet)
+router.register('proveedor',ProveedorViewSet)
 
